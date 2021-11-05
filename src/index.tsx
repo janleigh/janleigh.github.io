891b95d4687fe10738bd5840003dc1de79c5d852
TheRealKizu <therealkizu@protonmail.com>
2021-11-05 20:51:22 +0800

    chore: dep cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
+import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "bulma/css/bulma.css";
 import "./styles/scss/index.scss";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <React.StrictMode>
         <App/>
-    </BrowserRouter>,
+    </React.StrictMode>,
     document.getElementById("root")
 );
