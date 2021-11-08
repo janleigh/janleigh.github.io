ff64a5e355a697263373a275f66979129a042d58
TheRealKizu <therealkizu@protonmail.com>
2021-11-08 12:32:00 +0800

    feat: commissions section

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -14,6 +14,22 @@ export const Projects = () => {
                         <ProjectCard repository="therealkizu.github.io" />
                     </div>
                 </div>
+                <h1>Commissions</h1>
+                <div className="columns">
+                    <div className="column is-full">
+                        <p
+                            style={{ 
+                                textAlign: "center",
+
+                                color: "#fbf1c7",
+                                fontSize: "1.2rem",
+                            }}
+                        >Hm. Well, this is awkward. There seems nothing to be <a target="_blank" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">here</a>.
+                        <br/>
+                        I wish I had commissions. 😔
+                        </p>
+                    </div>
+                </div>
             </section>
         </div>
     )
