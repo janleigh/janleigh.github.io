b02b59435d420719ff0d335c274ff7a9538ea1bb
TheRealKizu <therealkizu@protonmail.com>
2021-12-12 09:52:40 +0800

    fix: 404 page homepage link

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const NotFound = () => {
@@ -8,7 +7,7 @@ export const NotFound = () => {
                 <h1 className="not-found-title">404</h1>
                 <p className="not-found-description">
                     The page you are looking for does not exist.<br/>
-                    You can always go back to the homepage by clicking <Link to="/how-did-you-find-this" target="_blank" className="link">me!</Link>
+                    You can always go back to the homepage by clicking <Link to="introduction" target="_blank" className="link">me!</Link>
                 </p>
             </div>
         </section>    
